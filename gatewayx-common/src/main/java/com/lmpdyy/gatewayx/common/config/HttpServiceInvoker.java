087e3594b55fe935b86b6073ce19d45b80f7e1ee
mpfei7416 <mpfei7416@vip.163.com>
2023-09-15 10:30:04 +0800

    http dubbo 服务类型调用模型

---
+++ gatewayx-common/src/main/java/com/lmpdyy/gatewayx/common/config/HttpServiceInvoker.java
@@ -0,0 +1,10 @@
+package com.lmpdyy.gatewayx.common.config;
+
+/**
+ * @ClassName HttpServiceInvoker
+ * @description: http协议的注册服务调用模型类
+ * @author: nxlea
+ * @create: 2023-09-15 10:24
+ */
+public class HttpServiceInvoker  extends  AbstractServiceInvoker {
+}
